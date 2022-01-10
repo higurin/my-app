527ff45fbc79aebc36fc0e043db75d21db81577b
樋口梨花 <rika@higuchirikanoMacBook-puro.local>
2022-01-11 01:14:33 +0900

    TODOをカスタムフックから取得する

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,4 @@
-import React, { useState, useEffect } from "react";
-
-// モックサーバーとの通信のため axios をimport
-import axios from "axios";
-
-// ローカルに用意したモックサーバーのURL
-const todoDataUrl = "http://localhost:3100/todos";
-
+import { useTodo } from "../hooks/useTodo";
 
 // todoTitle コンポーネントを作成
 const TodoTitle = ({ title, as }) => {
@@ -31,7 +24,6 @@ const TodoList = ({ todoList }) => {
     <ul>
       {todoList.map((todo) => (
         // TodoItem に一位なIDを key属性の値として付与
-        // todoList から取り出した todo を子コンポーネントへ props で渡す
         <TodoItem todo={todo} key={todo.id} />
       ))}
     </ul>
@@ -40,29 +32,9 @@ const TodoList = ({ todoList }) => {
 
 
 function App() {
-  // todoList は現在のTODOの状態
-  // setTodoList は現在の todoList の状態を更新するための関数
-  // todoList の初期値に空の配列をセット
-  const [todoList, setTodoList] = useState([]);
+  // useTodo() カスタムフックで作成した todoList を利用できるようにする
+  const { todoList } = useTodo();
 
-  // useEffect() を利用することでコンポーネントのマウント後に処理を実行
-  // async/await で非同期処理
-  useEffect(() => {
-    const fetchData = async () => {
-
-      // get は外部から情報を取得する基本メソッド
-      // get の引数にURLを入れると、URLに対してGETリクエストを送信
-      // リクエスト後に戻ってくる値は全て response に保存される
-      const response = await axios.get(todoDataUrl);
-
-      // 戻された値について useState を利用して、
-      // todoList の現在の値としてセットする
-      setTodoList(response.data);
-    };
-    fetchData();
-  }, []);
-
-  // console.logでコンソールに取得したTODOリストの情報を表示してみる
   console.log("TODOリスト:", todoList);
 
   // filter() を利用して「TODOの状態が未完了」の要素をもつ新しい配列を作成
